3ad73018599c46cebddc1ba8878331f20da3eeb0
Piers <piersmg@gmail.com>
2019-07-24 11:09:29 +0100

    Homepage changes and mobile fixes, new empower and mi pages

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//      var objHeight = 0
+//      var imgHeight = $('.preview-image').height();
+//      // $.each($('.preview').children(), function() {
+//      //        objHeight += $(this).height();
+//      // });
+//      $('.preview').height(imgHeight);
+// })
+
+// var $child = $('.image-container');
+// var $parent = $('.tabbed_title');
+
+// $("#radioButton").change(function () {
+//     var childHeight = $child.height();
+//     $parent.height(childHeight);
+// });
