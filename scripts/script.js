29542e1dd468150cd34510f983d3b18d083ba38a
Piers <piersmg@gmail.com>
2019-07-25 11:00:04 +0100

    Page transition from homepage to sub pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 $(".transition").click(function(e) {
 	e.preventDefault();
   var linkUrl = $(this).attr('href');
-  setTimeout(function(url) { window.location = url; }, 500, linkUrl);
+  setTimeout(function(url) { window.location = url; }, 300, linkUrl);
   $('.stripe').toggleClass('transition-active');
+});
+
+$(function() {
+	var referrer = document.referrer;
+  if (referrer == 'https://www.piersgarthwaite.com') {
+  	$('.pink-cover').animate(
+    {width: '0px'},
+    {height: '0px'},
+    {top: '-200px'},
+    {right: '45px'}, 900);
+  }
 });
