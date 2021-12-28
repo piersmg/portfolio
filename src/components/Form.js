f87b182ed3542e7d490215365ed25cef8a496920
piersmg <piersgarthwaite@hotmail.co.uk>
2021-12-28 17:24:44 +0000

    Adding recaptcha to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 const Form = () => {
 	return (
 		<div className="flex flex-col justify-center items-center w-full md:max-w-[500px]">
-			<form className="w-full" name="contact" action="/success" method="POST" data-netlify="true">
+			<form className="w-full" name="contact" action="/success" method="POST" data-netlify-recaptcha="true" netlify>
 				<div className="relative mb-8">
 					<input placeholder=" " type="text" name="name" />   
 					<label htmlFor="name">Name: </label>
@@ -14,6 +14,7 @@ const Form = () => {
 					<textarea placeholder=" " name="message"></textarea>
 					<label htmlFor="message">Message: </label>
 				</div>
+				<div data-netlify-recaptcha="true"></div>
 				<div>
 					<button className="text-2xl px-4 py-2 bg-secondary text-white focus:bg-primary hover:bg-primary dark:focus:bg-white dark:hover:bg-white focus:text-secondary hover:text-secondary" type="submit">Send</button>
 				</div>
